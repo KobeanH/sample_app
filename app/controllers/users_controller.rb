80c8809eb1af4258738e69f858c6900710b519dd
KobeanH <hirofumi.kobean102520@gmail.com>
2022-05-02 08:57:47 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -53,14 +54,6 @@ before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
     
         # beforeアクション
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
     
         # 正しいユーザーかどうか確認
     def correct_user
